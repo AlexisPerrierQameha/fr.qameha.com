e73460fd83c12598bd142ca949cdfe79672ee46a
Darjeelink <alexis.perrier@gmail.com>
2010-02-28 15:26:10 +0100

    gitignore

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :user, 'spock'
 set :git_account, 'darjeelink'
 set :scm_passphrase, "galactus88260" #This is your custom users password
 default_run_options[:pty] = true
-set :repository,  "git@github.com:darjeelink/qameha.com.git"
+set :repository,  "git@github.com:darjeelink/fr.qameha.com.git"
 set :scm, "git"
 set :user, "spock"
 ssh_options[:forward_agent] = true
@@ -24,14 +24,14 @@ set :use_sudo, false
 
 # Avoid keeping the database.yml configuration in git.
 task :copy_database_yml, :roles => :app do
-  run "cp /home/spock/qameha.com/database.yml #{release_path}/config/database.yml"
+  run "cp /home/spock/fr.qameha.com/database.yml #{release_path}/config/database.yml"
 end
 
 
-set :server_hostname, 'qameha.com'
+set :server_hostname, 'fr.qameha.com'
 set :location, "204.232.206.121"
 set :rails_env, "production"
-set :deploy_to, "/home/spock/qameha.com/"
+set :deploy_to, "/home/spock/fr.qameha.com/"
 set :port, 4800
 
 role :app, location
